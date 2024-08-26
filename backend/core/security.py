5d0176473f6821423a1bb0c32f202be002ffd320
garamgim <garamgim@gmail.com>
2024-08-26 22:25:10 +0900

    [#6] 현재 로그인한 유저 반환 기능

--- backend/core/security.py
+++ backend/core/security.py
@@ -13,13 +13,13 @@ def hash_password(password):
     return bcrypt_context.hash(password)
 
 def create_access_token(login_id: str, role: str):
-    payload = {'login_id': login_id, 'role': role, 'category': 'access'}
+    payload = {'sub': login_id, 'role': role, 'category': 'access'}
     expiration_minutes = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     payload.update({'exp': expiration_minutes})
     return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm=settings.ENCODE_ALGORITHM)
 
 def create_refresh_token(login_id: str, role: str):
-    payload = {'login_id': login_id, 'role': role, 'category': 'refresh'}
+    payload = {'sub': login_id, 'role': role, 'category': 'refresh'}
     expiration_minutes = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_REFRESH_TOKEN_EXPIRE_MINUTES)
     payload.update({'exp': expiration_minutes})
     return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm=settings.ENCODE_ALGORITHM)
--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,4 +1,13 @@
+from models import Member
 from core.db import Session
+from typing import Annotated
+from jose import jwt, JWTError
+from core.config import settings
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/login')
+
 
 def get_db():
     db = Session()
@@ -6,3 +15,30 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+async def get_current_user(session: Depends(get_db), token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.ENCODE_ALGORITHM])
+        login_id: str = payload.get('sub')
+        token_category: str = payload.get('category')
+
+        if login_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
+
+        if token_category != 'access':
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='Incorrect token type.')
+
+        member = session.query(Member).filter(Member.login_id == login_id).first()
+
+        if not member:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail='Member does not exist.')
+
+        return member
+
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')

a785a31469526bbb578fa56b345820f24cd76cb3
박수진 <tnwlssla20@naver.com>
2024-08-26 17:40:46 +0900

    [#7] feature: 회원가입 api 구현

---
+++ backend/api/routes/members.py
@@ -0,0 +1,23 @@
+from fastapi.params import Depends
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+from core.db import get_db
+from fastapi import APIRouter
+from schema import MemberCreate
+
+app = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+
+@app.post("/signup")
+async def signup(member: MemberCreate, session: Session = Depends(get_db)):
+    existing_member = crud.get_member_by_login_id(session, member.login_id)
+    if existing_member:
+        raise HTTPException(status_code=409, detail="이미 동일한 아이디의 회원이 존재합니다.")
+
+    crud.create_member(session, member)
+
+    return HTTPException(status_code=200, detail="회원가입이 완료되었습니다.")

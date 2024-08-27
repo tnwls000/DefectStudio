e49e35754c81d3a27de2b669ae3c19fd801349ce
박수진 <tnwlssla20@naver.com>
2024-08-27 15:40:23 +0900

    [#11] refactor: 회원가입 DTO 유효성 검사 적용

--- backend/schema.py
+++ backend/schema.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
 from enums import Role
 
 class MemberCreate(BaseModel):
-    login_id: str
-    password: str
-    name: str
-    nickname: str
+    login_id: str = Field(..., min_length=3, max_length=50)
+    password: str = Field(pattern=r'^\d*$') # TODO: 정규식 수정
+    name: str = Field(..., min_length=3, max_length=100)
+    nickname: str = Field(..., min_length=3, max_length=50, )
     email: EmailStr
-    role: Role
-    token_quantity: int
-    department_id: int
+    role: Role = Field(default=Role.department_member)
+    department_id: int = Field(...)

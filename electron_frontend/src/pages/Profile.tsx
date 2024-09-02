57f8e0d3e2e24fa6135c7c65770e67979e226268
SnowySparks <originpc@naver.com>
2024-09-02 16:05:39 +0900

    [#35] ui: 영역별 정보에 대한 html 요소 분리
    #S11P21S001-214 HTML틀 변형

--- electron_frontend/src/pages/Profile.tsx
+++ electron_frontend/src/pages/Profile.tsx
@@ -23,86 +23,81 @@ const dummyProfile: ProfileProps = {
 const Profile = () => {
   const userInfo = dummyProfile;
   return (
-    <>
-      <div className="w-full max-w-5xl bg-white py-10 px-12 rounded-[20px] mx-auto border border-gray-300 shadow-md h-full overflow-y-auto custom-scrollbar">
-        <div className="w-[90%] max-w-[1156px] mx-auto mt-8">
-          <div className="rounded-[20px] bg-white border border-[#e0e0e0] shadow-md p-6">
-            <div className="flex flex-col lg:flex-row">
-              <div className="flex flex-col items-center">
-                <svg
-                  width={141}
-                  height={141}
-                  viewBox="0 0 141 141"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="mb-4"
-                  preserveAspectRatio="xMidYMid meet"
-                >
-                  <path
-                    d="M139 70.5C139 108.332 108.332 139 70.5 139C32.6685 139 2 108.332 2 70.5C2 32.6685 32.6685 2 70.5 2C108.332 2 139 32.6685 139 70.5Z"
-                    fill="#C4C4C4"
-                    stroke="#8A2BE2"
-                    strokeWidth={4}
-                  />
-                </svg>
-                <p className="text-3xl text-black">Profile</p>
+    <div>
+      <div className="flex justify-center items-center h-[calc(100vh-60px)] bg-gray-100 p-4 overflow-hidden">
+        <div className="w-full max-w-5xl bg-white py-10 px-12 rounded-[20px] mx-auto border border-gray-300 shadow-md h-full overflow-y-auto custom-scrollbar">
+          {/* 프로필 헤더 */}
+          <p className="text-3xl text-black text-left mb-8">Profile</p>
+          <section className="flex items-center justify-between mb-8 relative">
+            <div className="flex flex-col md:flex-row items-center md:items-start">
+              <svg
+                width={141}
+                height={141}
+                viewBox="0 0 141 141"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mb-4 md:mb-0"
+                preserveAspectRatio="xMidYMid meet"
+              >
+                <path
+                  d="M139 70.5C139 108.332 108.332 139 70.5 139C32.6685 139 2 108.332 2 70.5C2 32.6685 32.6685 2 70.5 2C108.332 2 139 32.6685 139 70.5Z"
+                  fill="#C4C4C4"
+                  stroke="#8A2BE2"
+                  stroke-width={4}
+                />
+              </svg>
+              <div className="ms-8">
+                <p className="text-xl text-left text-black">Nickname</p>
+                <p className="text-base text-left text-black mt-2">
+                  {userInfo.nickname}
+                </p>
+                <p className="text-xl text-left text-black mt-4">Name</p>
+                <p className="text-base text-left text-black mt-2">
+                  {userInfo.name}
+                </p>
               </div>
-              <div className="lg:ml-10 mt-8 lg:mt-0 flex-1">
-                <div className="flex flex-col space-y-4">
-                  <div>
-                    <p className="text-xl text-black">Nickname</p>
-                    <p className="text-base text-black mt-1">
-                      삼성에들어가고싶은김싸피
-                    </p>
-                  </div>
-                  <div>
-                    <p className="text-xl text-black">Name</p>
-                    <p className="text-base text-black mt-1">김싸피</p>
-                  </div>
-                  <div>
-                    <p className="text-3xl text-black">Information</p>
-                    <div className="border border-[#3A3A5A] rounded-md p-2 mt-2">
-                      <p className="text-base text-black">Department</p>
-                      <p className="text-base text-black mt-1">
-                        Samsung Electronic Co. DX
-                      </p>
-                    </div>
-                    <div className="border border-[#3A3A5A] rounded-md p-2 mt-4">
-                      <p className="text-base text-black">Email</p>
-                      <p className="text-base text-black mt-1">
-                        ssafy@ssafy.com
-                      </p>
-                    </div>
-                    <div className="border border-[#3A3A5A] rounded-md p-2 mt-4">
-                      <p className="text-base text-black">Role</p>
-                      <p className="text-base text-black mt-1">
-                        Samsung Electronic Co. DX
-                      </p>
-                    </div>
-                  </div>
-                  <button className="w-[101px] h-10 bg-[#8a2be2] rounded-[10px] text-xl text-white mt-6">
-                    Edit
-                  </button>
+            </div>
+          </section>
+
+          {/* 다른 세부정보 */}
+          <section className="flex items-center justify-between mb-8 relative">
+            <div>
+              <div className="relative w-full mb-4">
+                <p className="text-base text-left text-black mb-1">
+                  Department
+                </p>
+                <div className="w-full h-10 p-2 border border-[#3A3A5A] rounded-lg">
+                  <p className="text-base text-left text-black w-[80vw] ">
+                    Samsung Electronic Co. DX
+                  </p>
+                </div>
+              </div>
+              <div className="relative w-full mb-4">
+                <p className="text-base text-left text-black mb-1">Email</p>
+                <div className="w-full h-10 p-2 border border-[#3A3A5A] rounded-lg">
+                  <p className="text-base text-left text-black ">
+                    ssafy@ssafy.com
+                  </p>
+                </div>
+              </div>
+              <div className="relative w-full mb-4">
+                <p className="text-base text-left text-black mb-1">Role</p>
+                <div className="w-full h-10 p-2 border border-[#3A3A5A] rounded-lg">
+                  <p className="text-base text-left text-black">
+                    Samsung Electronic Co. DX
+                  </p>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="mt-8">
-            <p className="text-3xl text-black">Token Status</p>
-            <div className="border border-[#2c2c3a] rounded-[10px] bg-white p-4 mt-2">
-              <p className="text-base text-black">
-                Left total tokens: 12345000
-              </p>
-              <p className="text-base text-black mt-2">Usage Tokens Table</p>
-              <p className="text-base text-black mt-8">
-                여기에는 사용량에 따른 그래프 표를 보여줌
-              </p>
-            </div>
-          </div>
+          </section>
+          {/* 토큰 정보 */}
+          <section className="flex items-center justify-between mb-8 relative">
+            <p className="text-3xl text-black text-left mb-8">Token Status</p>
+            <div></div>
+          </section>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

0a7760178f1561523b4d9bee52ff7edbc183ee7d
garamgim <garamgim@gmail.com>
2024-09-02 22:35:27 +0900

    [#24] AI 서버 API Endpoint 정리  #S11P21S001-161 #in-progress

--- ai/main.py
+++ ai/main.py
@@ -1,7 +1,13 @@
+from sys import prefix
+
 import uvicorn
 from fastapi import FastAPI
-
-app = FastAPI()
+from api.main import api_router
+app = FastAPI(
+    prefix="/api"
+)
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8001)
+

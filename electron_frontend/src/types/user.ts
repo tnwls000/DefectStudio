08987dee55c944f4108985259dc3075714fea6b9
SnowyPacks <originpc@naver.com>
2024-09-03 00:11:20 +0900

    [#36] feature: userInfo에 대한 redux-toolkit Store작성

--- electron_frontend/src/types/user.ts
+++ electron_frontend/src/types/user.ts
@@ -1,4 +1,4 @@
-export type signUpFormType = {
+export interface signUpFormType {
   login_id: string;
   password: string;
   name: string;
@@ -6,4 +6,9 @@ export type signUpFormType = {
   email: string;
   role: "super_admin" | "department_member" | "department_admin";
   department_id: number;
+}
+
+export type userInfo = Omit<signUpFormType, "password"> & {
+  isLogined: boolean;
+  token: string;
 };

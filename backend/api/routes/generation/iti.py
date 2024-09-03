bbc04a8ef4fd63f7ab1a7a52b0c4df647561b4e6
garamgim <garamgim@gmail.com>
2024-09-03 17:58:48 +0900

    [#40] feature: ITI BE 서버 API 진행중  #S11P21S001-164 #in-progress

--- backend/api/routes/generation/iti.py
+++ backend/api/routes/generation/iti.py
@@ -1,31 +1,48 @@
-import json
+import mimetypes
+import re
+from pathlib import Path
 
 import requests
-from fastapi import APIRouter, Response, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Response
 from starlette.responses import JSONResponse
 
-from api.routes.generation.schema import TTIRequest
+from api.routes.generation.schema import ITIRequest
 from core.config import settings
 from enums import GPUEnvironment
 from utils.local_io import save_image_files
 from utils.s3 import upload_files
 
 router = APIRouter(
-    prefix="/txt-to-img",
+    prefix="/img-to-img",
 )
 
-
 @router.post("/{gpu_env}")
-def text_to_image(gpu_env: GPUEnvironment, request: TTIRequest):
+def text_to_image(gpu_env: GPUEnvironment, request: ITIRequest):
     # TODO : 유저 인증 확인 후 토큰 사용
     payload_dict = request.model_dump()
+
+    input_path = payload_dict.get("input_path")
+
+    image_files = [file for file in Path(input_path).glob("*") if re.search(r"\.(png|jpg|jpeg|jfif)$", file.suffix, re.IGNORECASE)]
+    if not image_files:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="유효한 이미지가 없습니다.")
 
     # 로컬 GPU 사용 시 이미지 저장 경로는 필수
     if gpu_env == GPUEnvironment.local and not payload_dict.get("output_path"):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="로컬 파일 경로를 지정해주세요.")
 
-    payload = json.dumps(payload_dict)
-    response = requests.post(settings.AI_SERVER_URL + "/txt-to-img", data=payload)
+    files = []
+    for image_file in image_files:
+        with open(image_file, "rb") as f:
+            image_bytes = f.read()
+
+            mime_type, _ = mimetypes.guess_type(image_file.name)
+            if mime_type is None:
+                mime_type = "application/octet-stream"
+
+            files.append(("images", (image_file.name, image_bytes, mime_type)))
+
+    response = requests.post(settings.AI_SERVER_URL + "/img-to-img", files=files)
 
     if response.status_code != 200:
         return Response(status_code=response.status_code, content=response.content)
--- backend/api/routes/generation/schema.py
+++ backend/api/routes/generation/schema.py
@@ -4,14 +4,49 @@ from pydantic import BaseModel, Field
 
 class TTIRequest(BaseModel):
     model: str = Field("CompVis/stable-diffusion-v1-4")
-    prompt: str = Field(...)
+
+    # 프롬프트
+    prompt: str = Field(..., description="이미지를 생성할 텍스트 프롬프트")
     negative_prompt: Optional[str] = Field(None)
+
+    # 이미지 사이즈
     width: Optional[int] = Field(512)
     height: Optional[int] = Field(512)
-    num_inference_steps: Optional[int] = Field(50)
-    guidance_scale: Optional[float] = Field(7.5)
-    seed: Optional[int] = Field(-1)
+
+    # 추론 관련 변수
+    num_inference_steps: Optional[int] = Field(50, ge=1, le=100, description="추론 단계 수")
+    guidance_scale: Optional[float] = Field(7.5, ge=1.0, le=20.0, description="가이던스 스케일")
+    seed: Optional[int] = Field(-1, description="이미지 생성 시 사용할 시드 값 (랜덤 시드: -1)")
+
+    # 이미지 갯수
     num_images_per_prompt: Optional[int] = Field(1)
-    batch_count: Optional[int] = Field(1)
-    batch_size: Optional[int] = Field(1)
-    output_path: Optional[str] = Field(None)
+    batch_count: Optional[int] = Field(1, ge=1, le=10, description="호출할 횟수")
+    batch_size: Optional[int] = Field(1, ge=1, le=10, description="한 번의 호출에서 생성할 이미지 수")
+
+    # 경로
+    output_path: Optional[str] = Field(None, description="이미지를 저장할 로컬 경로")
+
+
+class ITIRequest(BaseModel):
+    model: str = Field("CompVis/stable-diffusion-v1-4")
+    # image:
+
+    # 프롬프트
+    prompt: str = Field(..., description="이미지를 생성할 텍스트 프롬프트")
+    negative_prompt: Optional[str] = Field(None)
+
+    # 추론
+    num_inference_steps: Optional[int] = Field(50, ge=1, le=100, description="추론 단계 수")
+    guidance_scale: Optional[float] = Field(7.5, ge=1.0, le=20.0, description="모델이 텍스트 프롬프트에 얼마나 충실하게 이미지를 생성할지에 대한 수치 (10.0=프롬프트를 강하게 따름, 0.0=프롬프트 벗어남)")
+    strength: Optional[float] = Field(0.5, ge=0.0, le=1.0, description="초기 이미지와 얼마나 다르게 생성할지에 대한 수치 (1.0=초기 이미지 무관, 0.0=초기 이미지 유지)")
+
+    # 이미지 갯수
+    num_images_per_prompt: Optional[int] = Field(1)
+    batch_count: Optional[int] = Field(1, ge=1, le=10, description="호출할 횟수")
+    batch_size: Optional[int] = Field(1, ge=1, le=10, description="한 번의 호출에서 생성할 이미지 수")
+
+    # 경로
+    input_path: str = Field(..., description="이미지를 가져올 로컬 경로")
+    output_path: Optional[str] = Field(None, description="이미지를 저장할 로컬 경로")
+
+

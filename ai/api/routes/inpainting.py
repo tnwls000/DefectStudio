ba021afc0ad3a6ee6e3d2f58426ead23858c13ac
garamgim <garamgim@gmail.com>
2024-09-06 23:39:37 +0900

    [#50] refactor: AI Generation API 이미지 변환 코드 리팩터링

--- ai/api/routes/inpainting.py
+++ ai/api/routes/inpainting.py
@@ -1,66 +1,41 @@
 import base64
 from io import BytesIO
-from typing import List
 
 import PIL.Image
 import torch
 from diffusers import AutoPipelineForInpainting
-from fastapi import APIRouter, Request, Form, UploadFile, File
+from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
-
-from api.routes.schema import ITIRequestForm
 
 router = APIRouter(
     prefix="/inpainting",
 )
+
 
 @router.post("")
 async def inpainting(
         request: Request
 ):
     form = await request.form()
-    print(form)
-    print(form.getlist("images"))
 
     model = form.get("model")
     prompt = form.get("prompt")
     negative_prompt = form.get("negative_prompt")
-    num_inference_steps=int(form.get("num_inference_steps", 50))
-    guidance_scale=float(form.get("guidance_scale", 7.5))
-    strength=float(form.get("strength", 0.5))
-    num_images_per_prompt=int(form.get("num_images_per_prompt", 1))
-    batch_count=int(form.get("batch_count", 1))
-    batch_size=int(form.get("batch_size", 1))
+    num_inference_steps = int(form.get("num_inference_steps"))
+    guidance_scale = float(form.get("guidance_scale"))
+    strength = float(form.get("strength"))
+    num_images_per_prompt = int(form.get("num_images_per_prompt"))
+    init_image_files = form.getlist("init_image")
+    mask_image_files = form.getlist("mask_image")
+    batch_count = int(form.get("batch_count"))
+    batch_size = int(form.get("batch_size"))
+
+    init_image_list = [PIL.Image.open(BytesIO(await file.read())) for file in init_image_files]
+    mask_image_list = [PIL.Image.open(BytesIO(await file.read())) for file in mask_image_files]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     inpaint_pipe = AutoPipelineForInpainting.from_pretrained(model, torch_dtype=torch.float16).to(device)
 
-    images = form.getlist("images")
-
-    init_image_list = []
-    mask_image_list = []
-
-    # PIL.Image로 변환
-    for file in images:
-        file_data = await file.read()
-        image_bytes_io = BytesIO(file_data)
-        image = PIL.Image.open(image_bytes_io)
-        # 파일 이름에 붙여준 prefix에 따라 구분한 후 분리
-        init_image_list.append(image) if file.filename[0] == 'i' else mask_image_list.append(image)
-
-    # Debugging output
-    print(f"Init Images: {len(init_image_list)}")
-    print(f"Mask Images: {len(mask_image_list)}")
-
-    # Ensure init and mask lists are not empty and have the correct corresponding pairs
-    if not init_image_list or not mask_image_list:
-        raise ValueError("Init images or mask images are missing.")
-
-    # Ensure the number of init images matches the number of mask images
-    if len(init_image_list) != len(mask_image_list):
-        raise ValueError("The number of initial images and mask images must match.")
-
-    # Generate inpainted images
     generated_image_list = inpaint_pipe(
         image=init_image_list,
         mask_image=mask_image_list,
@@ -71,7 +46,6 @@ async def inpainting(
         strength=strength,
         num_images_per_prompt=num_images_per_prompt,
     ).images
-
 
     encoded_images = []
 
--- ai/api/routes/iti.py
+++ ai/api/routes/iti.py
@@ -1,21 +1,16 @@
 import base64
-import os
 from io import BytesIO
-from typing import List
 
 import PIL.Image
 import torch
 from diffusers import StableDiffusionImg2ImgPipeline
-from fastapi import APIRouter, Request, Form, UploadFile, File
+from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
-
-from api.routes.schema import ITIRequestForm
 
 router = APIRouter(
     prefix="/img-to-img",
 )
 
-# os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 @router.post("")
 async def image_to_image(
@@ -23,30 +18,21 @@ async def image_to_image(
 ):
     form = await request.form()
 
-    model=form.get("model", "CompVis/stable-diffusion-v1-4")
-    prompt=form.get("prompt")
-    negative_prompt=form.get("negative_prompt")
-    num_inference_steps=int(form.get("num_inference_steps"))
-    guidance_scale=float(form.get("guidance_scale"))
-    strength=float(form.get("strength"))
-    num_images_per_prompt=int(form.get("num_images_per_prompt"))
-    batch_count=int(form.get("batch_count"))
-    batch_size=int(form.get("batch_size"))
+    model = form.get("model", "CompVis/stable-diffusion-v1-4")
+    prompt = form.get("prompt")
+    negative_prompt = form.get("negative_prompt")
+    num_inference_steps = int(form.get("num_inference_steps"))
+    guidance_scale = float(form.get("guidance_scale"))
+    strength = float(form.get("strength"))
+    num_images_per_prompt = int(form.get("num_images_per_prompt"))
+    batch_count = int(form.get("batch_count"))
+    batch_size = int(form.get("batch_size"))
 
-    # 파일 추출
     images = form.getlist("images")
+    image_list = [PIL.Image.open(BytesIO(await file.read())) for file in images]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     i2i_pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model, torch_dtype=torch.float16).to(device)
-
-    image_list = []
-
-    # PIL.Image로 변환
-    for file in images:
-        file_data = await file.read()
-        image_bytes_io = BytesIO(file_data)
-        image = PIL.Image.open(image_bytes_io)
-        image_list.append(image)
 
     generated_image_list = i2i_pipe(
         image=image_list,

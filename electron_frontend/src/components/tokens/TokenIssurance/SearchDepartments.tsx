61bc4ad18672ecebedaf8c277cd92f3d7c21eb1c
SnowyPacks <originpc@naver.com>
2024-09-07 02:56:27 +0900

    [#42] feature: 검색기능수정

--- electron_frontend/src/components/tokens/TokenIssurance/SearchDepartments.tsx
+++ electron_frontend/src/components/tokens/TokenIssurance/SearchDepartments.tsx
@@ -1,34 +1,31 @@
 import "./SearchDepartments.css";
 import { useQuery } from "@tanstack/react-query";
-import { Input } from "antd";
 import { useEffect, useState } from "react";
 import { getAllDepartments } from "../../../api/getAllDepartment";
 import { AxiosError, AxiosResponse } from "axios";
-import { Table } from "antd";
+import { Select, Space } from "antd";
 
 type departmentType = {
   department_id: number;
   department_name: string;
 };
 
-type TableDepartmentType = {
-  department_id: number;
-  department_name: string;
-  key: React.Key;
+type AntdSelectOptionType = {
+  value: number;
+  label: string;
 };
 
 const SearchDepartments = () => {
-  // 검색이름
-  const [searchDepartmentName, setSearchDepartmentName] = useState("");
-
-  // 검색 할 경우 보여줄 데이터
-  const [filteredData, setFilteredData] = useState<TableDepartmentType[]>([]);
-
   //부서 불러오기
-  const { data, isPending, isError, error } = useQuery<
+  const {
+    data = [],
+    isPending,
+    isError,
+    error,
+  } = useQuery<
     AxiosResponse<departmentType[]>,
     AxiosError,
-    TableDepartmentType[],
+    AntdSelectOptionType[],
     string[]
   >({
     queryKey: ["departments"],
@@ -36,40 +33,17 @@ const SearchDepartments = () => {
     select: (response) =>
       response.data.map((department) => {
         return {
-          department_id: department.department_id,
-          department_name: department.department_name,
+          value: department.department_id,
+          label: department.department_name,
           key: department.department_id,
         };
       }),
   });
 
-  // 검색어에 따라 필터링 되는 코드 작성
-  useEffect(() => {
-    if (data) {
-      setFilteredData(
-        data.filter((department) =>
-          department.department_name
-            .toLowerCase()
-            .includes(searchDepartmentName.toLowerCase())
-        )
-      );
-    }
-  }, [searchDepartmentName, data]);
-
-  // 테이블 필드명
-  const columns = [
-    {
-      title: "Department ID",
-      dataIndex: "department_id",
-      key: "department_id",
-    },
-    {
-      title: "Department Name",
-      dataIndex: "department_name",
-      key: "department_name",
-    },
-  ];
-
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const filteredOptions = data.filter(
+    (item) => !selectedItems.includes(item.value)
+  );
   //컴포넌트 출력
   return (
     <section className="token-issurance-department-container">
@@ -77,17 +51,23 @@ const SearchDepartments = () => {
         <p className="text-[20px] font-bold">
           Please select the departments you wish to grant the token to
         </p>
-        <Input
-          onChange={(e) => setSearchDepartmentName(e.target.value)}
-          className="h-[40px] mt-2"
-        />
       </div>
 
       <div className="">
         {isPending && <p>Loading...</p>}
         {isError && <p>{error.message}</p>}
         {data && (
-          <Table columns={columns} dataSource={data} rowKey="department_id" />
+          <Select
+            mode="multiple"
+            value={selectedItems}
+            onChange={setSelectedItems}
+            style={{ width: "100%" }}
+            options={filteredOptions.map((item) => ({
+              value: item.value,
+              label: item.label,
+            }))}
+            optionRender={(option) => <Space>{option.data.label}</Space>}
+          />
         )}
       </div>
     </section>

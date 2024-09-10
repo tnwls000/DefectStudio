3463a9ed025985385317e168665f7cc591c6fbb1
JaeHunJang <wuddlaa@gmail.com>
2024-09-11 01:42:22 +0900

    [#60] chore: 프로젝트 구조 추가
    #S11P21S001-285

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from .routes import auth, members, admin, departments
 from .routes.generation import main as generation
+from .routes.device import main as device
 
 api_router = APIRouter()
 
@@ -9,3 +10,4 @@ api_router.include_router(members.router)
 api_router.include_router(admin.router)
 api_router.include_router(generation.router)
 api_router.include_router(departments.router)
+api_router.include_router(device.router)

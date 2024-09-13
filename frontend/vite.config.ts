ec7cd66f314c803996dfb2ccbd05940f9bc81a32
JaeHunJang <wuddlaa@gmail.com>
2024-09-13 15:56:45 +0900

    delete: 환경 변경으로 인한 이전 작업 디렉터리 삭제
    Former-commit-id: 3799999aff40acafdbc997ee2f14be94f4364863

--- frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [react(), svgrPlugin(), tsconfigPaths()]
-});

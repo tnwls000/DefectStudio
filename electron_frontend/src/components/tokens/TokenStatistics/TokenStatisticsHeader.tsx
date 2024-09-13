2141eb44fe77ce7b6873ac0af92ada2213cb4947
SnowySparks <originpc@naver.com>
2024-09-13 15:10:28 +0900

    [#42] feature : API 코드 작성 및 Statistics 구축
    Former-commit-id: bf8bdded6d80f514b2ffb9a3847c4c1f76014154

--- electron_frontend/src/components/tokens/TokenStatistics/TokenStatisticsHeader.tsx
+++ electron_frontend/src/components/tokens/TokenStatistics/TokenStatisticsHeader.tsx
@@ -1,13 +1,22 @@
-import { Select } from 'antd';
 import SearchDepartments from '../common/SearchDepartments';
-const TokenStatisticsHeader = () => {
+
+interface TokenStatisticsHeaderProps {
+  selectedDepartment: number;
+  setSelectedDepartment: (departmentsId: number[]) => void;
+}
+
+const TokenStatisticsHeader = ({ selectedDepartment, setSelectedDepartment }: TokenStatisticsHeaderProps) => {
   return (
     <header className="flex flex-col token-content">
       <div className="flex flex-row justify-between">
         <p className="text-gray-300 dark:text-white font-samsung font-bold">Department</p>
 
         <div className="w-[70%]">
-          <SearchDepartments isMultiSelect={false} />
+          <SearchDepartments
+            isMultiSelect={false}
+            departmentsId={selectedDepartment}
+            setDepartmentsId={setSelectedDepartment}
+          />
         </div>
       </div>
     </header>

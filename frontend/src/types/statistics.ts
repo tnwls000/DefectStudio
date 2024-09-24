62889528cac26b1e457a0d3921c0cd0dc41ec2e0
SnowySparks <originpc@naver.com>
2024-09-24 14:21:23 +0900

    [#105] feature: 통계 API response Type 정의
    Former-commit-id: 52de7c642d518b9ac99782e4861444c6580b5599

---
+++ frontend/src/types/statistics.ts
@@ -0,0 +1,42 @@
+// 통계와 관련된 모든 Type 정의
+
+// 도구 목록
+export type UseType = 'text_to_image' | 'remove_background' | 'clip' | 'clean_up' | 'image_to_image';
+
+// 일별 이미지 수 - 일별 생성한 이미지 총 수
+export interface DailyImageCount {
+  create_date: string;
+  image_quantity: number;
+}
+
+// 도구별 사용 빈도
+export interface ToolFrequency {
+  use_type: UseType;
+  usage: number;
+}
+
+// 모델 별 사용 빈도
+export interface ModelFrequency {
+  model: string;
+  usage: number;
+}
+
+// 부서 내 사용자 별 이미지 생성 수
+export interface MemberImageCount {
+  member_id: number;
+  member_name: string;
+  image_quantity: number;
+}
+
+// 부서 내에서 토큰 분배
+export interface TokenDistribution {
+  distribute_date: string;
+  token_quantity: number;
+}
+
+// 부서 내 사용자별 토큰 사용 현황
+export interface DepartmentTokenUsage {
+  member_id: number;
+  member_name: string;
+  token_quantity: number;
+}

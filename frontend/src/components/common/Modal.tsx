7d36584db3e1ead2be54d06bb1fbd3299fe4ddd3
SnowySparks <originpc@naver.com>
2024-09-24 10:30:45 +0900

    [#100] fix : Modal background color fix
    Former-commit-id: b17b70382dfac7a706fa48ee9e34958870ca088e

--- frontend/src/components/common/Modal.tsx
+++ frontend/src/components/common/Modal.tsx
@@ -13,7 +13,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
     <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center ">
       <div className="relative bg-white dark:bg-slate-700 rounded-lg shadow-lg p-6 w-full max-w-[575px] h-auto mx-auto">
         <button
-          className="absolute top-2 right-2 dark:text-gray-200 text-gray-500 hover:text-gray-700 dark:hover:text-gray-500"
+          className="absolute top-2 right-2 dark:text-gray-200 text-gray-500 hover:text-gray-600 dark:hover:text-gray-500"
           onClick={onClose}
         >
           close

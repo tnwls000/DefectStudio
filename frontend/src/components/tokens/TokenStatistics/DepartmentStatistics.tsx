82f8516a8fcf7d9394206f6c942128af7eb23d31
SnowyPacks <originpc@naver.com>
2024-09-25 23:19:00 +0900

    [#105] feature: 부서별 Tool 사용통계 컴포넌트 구축
    Former-commit-id: 6cd0f554ecd94e4f6c16ebd28438f228d2d2b85c

--- frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
@@ -1,5 +1,6 @@
 import { Tabs, TabsProps } from 'antd';
 import DepartmentImageUsage from '@components/tokens/statistics/department/ImageUsage/DepartmentImageUsage';
+import DepartmentToolUsage from '../statistics/department/ToolUsage/DepartmentToolUsage';
 
 interface DepartmentStatisticsProps {
   department_id: number;
@@ -7,7 +8,8 @@ interface DepartmentStatisticsProps {
 
 const DepartmentStatistics = ({ department_id }: DepartmentStatisticsProps) => {
   const items: TabsProps['items'] = [
-    { key: 'ImageUage', label: 'ImageUage', children: <DepartmentImageUsage department_id={department_id} /> }
+    { key: 'ImageUage', label: 'ImageUage', children: <DepartmentImageUsage department_id={department_id} /> },
+    { key: 'ToolUsage', label: 'ToolUsage', children: <DepartmentToolUsage department_id={department_id} /> }
   ];
   return (
     <div className="token-content">

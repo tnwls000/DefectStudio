7bd3321cf35c67d59cf1d9ca260950e400c12c5e
garamgim <garamgim@gmail.com>
2024-09-27 17:52:39 +0900

    [#76] fix: Task Arguments에 이미지 존재 시 발생하는 오류 제거
     #S11P21S001-421 #in-progress

--- ai/schema.py
+++ ai/schema.py
@@ -4,9 +4,18 @@ from pydantic import BaseModel
 
 
 class CeleryTaskResponse(BaseModel):
-    task_name: str
+    task_name: Optional[str] = None
     task_status: str
-    task_arguments: dict
+    task_arguments: Optional[dict] = None
     result_data_type: Optional[str] = None
     result_data: Optional[Union[List[str], str]] = None
     message: Optional[str] = None
+
+    def __init__(self, **data):
+        # 불필요한 키 제거
+        if 'task_arguments' in data and data['task_arguments'] is not None:
+            keys_to_remove = ["images", "init_image_files", "mask_image_files", "masks"]
+            for key in keys_to_remove:
+                data['task_arguments'].pop(key, None)
+
+        super().__init__(**data)

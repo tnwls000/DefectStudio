c90c8594d6ce0e42edc17b4f0894d7d3566201dc
surina125 <jhyunsoo1125@gmail.com>
2024-09-27 09:46:09 +0900

    [#98] fix: txt2img 수정된 api연동
    Former-commit-id: 0a988bf81dd87a552656050267be25f90aeaf59e

--- frontend/src/components/generation/sidebar/Txt2ImgSidebar.tsx
+++ frontend/src/components/generation/sidebar/Txt2ImgSidebar.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import CreatePreset from '../presets/CreatePreset';
 import LoadPreset from '../presets/LoadPreset';
 import ModelParam from '../params/ModelParam';

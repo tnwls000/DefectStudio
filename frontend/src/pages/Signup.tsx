5685f47f6b55598622c10ccdcb62795848f30769
SnowySparks <originpc@naver.com>
2024-09-27 14:29:45 +0900

    [#120] fix: 회원가입 페이지 전반적 수정 - css 에러 픽스
    Former-commit-id: fc6998b6b99bc7c247443e6ba19ce3e4ca1411bc

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,176 +1,230 @@
-import { Form, Input, Button, message, Select } from 'antd';
 import { signUpFormType } from '../types/user';
-import { signupHTTP } from '../api/signupHTTP';
-import { useNavigate } from 'react-router-dom';
 import { getAllDepartments } from '../api/department';
 import { useQuery } from '@tanstack/react-query';
 import { departmentType } from '../api/department';
 import { AxiosResponse } from 'axios';
+import { useForm } from 'react-hook-form';
 
-const initialValues: signUpFormType = {
+type FormType = signUpFormType & { verifyCode: string };
+
+const defaultValues: FormType = {
   login_id: '',
   password: '',
   name: '',
   nickname: '',
   email: '',
-  department_id: 0
+  department_id: 0,
+  verifyCode: ''
 };
 
 const Signup = () => {
-  const navigate = useNavigate();
-  // 제출 코드
-  const onSubmit = async (data: signUpFormType) => {
-    console.log(data);
-    try {
-      await signupHTTP(data);
-      form.resetFields();
-      message.success('Your registration request has been completed. Please contact the administrator for approval.');
-      navigate('/login');
-    } catch (error) {
-      message.error('Failed to sign up. Please try again later.');
-    }
-  };
-
   // 부서 정보 가져오기
-  const { data, isPending, isError, error } = useQuery<
-    AxiosResponse<departmentType[]>,
-    Error,
-    departmentType[],
-    (string | number)[]
-  >({
+  const {
+    data: department_list,
+    isPending,
+    isError,
+    error
+  } = useQuery<AxiosResponse<departmentType[]>, Error, departmentType[], (string | number)[]>({
     queryKey: ['departments'],
     queryFn: getAllDepartments,
     select: (data) => data.data
   });
 
-  const [form] = Form.useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isValid, errors }
+  } = useForm<FormType>({ mode: 'onChange', defaultValues });
 
   // 조건부 렌더링
   if (isPending) return <div>Loading...</div>;
   if (isError) return <div>Error: {error?.message}</div>;
-
-  return (
-    <div className="w-full h-full min-h-[1024px] relative overflow-hidden bg-white dark:bg-gray-800">
-      <p className="absolute left-1/2 top-8 transform -translate-x-1/2 text-2xl sm:text-3xl font-black text-center text-black dark:text-white">
-        Welcome to Defect Studio
-      </p>
-      <section className="absolute top-[100px] left-1/2 transform -translate-x-1/2">
-        <Form
-          form={form}
-          onFinish={onSubmit}
-          layout="vertical"
-          style={{ width: '90%', minWidth: '400px' }}
-          initialValues={initialValues}
-        >
-          <Form.Item
-            label="Login ID"
-            name="login_id"
-            rules={[
-              { required: true, message: 'Login ID is required' },
-              { min: 3, message: 'Login ID must be at least 3 characters' },
-              { max: 16, message: 'Login ID must be at most 16 characters' },
-              {
-                pattern: /^[a-zA-Z0-9]+$/,
-                message: 'Login ID can only contain letters and numbers'
-              }
-            ]}
+  else
+    return (
+      <div className="w-full h-full relative overflow-hidden bg-white dark:bg-gray-800">
+        <p className="absolute left-1/2 top-8 transform -translate-x-1/2 text-2xl sm:text-3xl font-black text-center text-black dark:text-white">
+          Welcome to Defect Studio
+        </p>
+        <section className="flex flex-col items-center mt-[120px] mb-10">
+          <form
+            onSubmit={handleSubmit((data) => {
+              console.log(data);
+            })}
           >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              { required: true, message: 'Password is required' },
-              { min: 8, message: 'Password must be at least 8 characters' },
-              { max: 20, message: 'Password must be at most 20 characters' },
-              {
-                pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/,
-                message: 'Password must include alphabet, number Each'
-              }
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            label="Name"
-            name="name"
-            rules={[
-              { required: true, message: 'Name is required' },
-              { max: 15, message: 'Name must be at most 15 characters' },
-              {
-                pattern: /^[a-zA-Z0-9가-힣]+$/,
-                message: 'Name can only contain letters, numbers, and Korean characters'
-              }
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Nickname"
-            name="nickname"
-            rules={[
-              { required: true, message: 'Nickname is required' },
-              { max: 15, message: 'Nickname must be at most 15 characters' },
-              {
-                pattern: /^[a-zA-Z0-9가-힣]+$/,
-                message: 'Nickname can only contain letters, numbers, and Korean characters'
-              }
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              { required: true, message: 'Email is required' },
-              { type: 'email', message: 'Invalid email address' }
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Department"
-            name="department_id"
-            rules={[
-              { required: true, message: 'Please Select Department' },
-              {
-                validator: (_, value) => {
-                  if (value === 0) {
-                    return Promise.reject('Please Select Department');
+            {/* username (user_id) */}
+            <div className="h-[85px]">
+              <label
+                htmlFor="username"
+                className="mb-2 text-[16px] font-samsung font-bold text-gray-900 dark:text-white"
+              >
+                UserName
+              </label>
+              <input
+                type="text"
+                id="username"
+                {...register('login_id', {
+                  required: 'Username is required',
+                  minLength: {
+                    value: 3,
+                    message: 'Username must be at least 4 characters long'
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: 'Username must be less than 16 characters long'
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9]*$/,
+                    message: 'Username must contain only alphabets and numbers'
                   }
-                  return Promise.resolve();
-                }
-              }
-            ]}
-            initialValue={1}
-          >
-            <Select>
-              <Select.Option value={0} disabled={true}>
-                Select Department
-              </Select.Option>
-              {data.map((department) => (
-                <Select.Option key={department.department_id} value={department.department_id}>
-                  {department.department_name}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="w-[100] min-w-[400px] my-5 max-w-[400px] h-[53px] rounded-[10px] bg-[#6200ea] text-lg font-black text-center text-white focus:outline-none"
-            >
-              Request Signup
-            </Button>
-          </Form.Item>
-        </Form>
-      </section>
-    </div>
-  );
+                })}
+                className=" mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[400px] h-[50px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Username"
+                required
+              />
+              {errors.login_id && <p className="text-red-500 text-xs mt-1">{errors.login_id.message}</p>}
+            </div>
+
+            {/* password */}
+            <div className="mt-5 h-[85px]">
+              <label
+                htmlFor="password"
+                className="mb-2 text-[16px] font-samsung font-bold text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                {...register('password', {
+                  required: 'Password is required',
+                  minLength: {
+                    value: 8,
+                    message: 'Password must be at least 8 characters long'
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: 'Password must be less than 20 characters long'
+                  },
+                  pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/,
+                    message: 'Password must contain only alphabets and numbers'
+                  }
+                })}
+                className=" mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[400px] h-[50px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Password"
+                required
+              />
+              {errors.password && <p className="text-red-500 text-xs mt-1">{errors.password.message}</p>}
+            </div>
+            {/* Name */}
+            <div className="mt-5 h-[85px]">
+              <label htmlFor="name" className="mb-2 text-[16px] font-samsung font-bold text-gray-900 dark:text-white">
+                Name
+              </label>
+              <input
+                type="name"
+                id="name"
+                {...register('name', {
+                  required: 'Name is required',
+                  maxLength: {
+                    value: 15,
+                    message: 'Name must be less than 15 characters long'
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9가-힣]+$/,
+                    message: 'Name can only contain letters, numbers, and Korean characters'
+                  }
+                })}
+                className=" mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[400px] h-[50px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Name"
+                required
+              />
+              {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>}
+            </div>
+            {/* 닉네임 */}
+            <div className="mt-5 h-[85px]">
+              <label
+                htmlFor="nickname"
+                className="mb-2 text-[16px] font-samsung font-bold text-gray-900 dark:text-white"
+              >
+                Nickname
+              </label>
+              <input
+                type="nickname"
+                id="nickname"
+                {...register('nickname', {
+                  required: 'Nickname is required',
+
+                  maxLength: {
+                    value: 15,
+                    message: 'Nickname must be less than 15 characters long'
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9가-힣]+$/,
+                    message: 'Nickname can only contain letters, numbers, and Korean characters'
+                  }
+                })}
+                className=" mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[400px] h-[50px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Nickname"
+                required
+              />
+            </div>
+            {/* 이메일 */}
+            <div className="mt-5 h-[85px]">
+              <label htmlFor="email" className="mb-2 text-[16px] font-samsung font-bold text-gray-900 dark:text-white">
+                Email
+              </label>
+
+              <input
+                type="email"
+                id="email"
+                {...register('email', {
+                  required: 'Email is required',
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: 'Please enter a valid email address'
+                  }
+                })}
+                className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-[400px] h-[50px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+                placeholder="Email"
+                required
+              />
+              {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>}
+            </div>
+
+            <div className="mt-5">
+              <label
+                htmlFor="department_id"
+                className="mb-2 text-[16px] font-samsung font-bold text-gray-900 dark:text-white"
+              >
+                Department
+              </label>
+              <select
+                name=""
+                id=""
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-[50px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                {department_list?.map((department) => (
+                  <option key={department.department_id} value={department.department_id}>
+                    {department.department_name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="w-[400px] h-[50px] bg-blue-500 text-white rounded-lg mt-10 disabled:opacity-50"
+                disabled={!isValid || isSubmitting}
+              >
+                Verify Email & Sign Up
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
+    );
 };
 
 export default Signup;

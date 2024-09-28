8c0e954e84ae1ed661927246e15828a678bada73
surina125 <jhyunsoo1125@gmail.com>
2024-09-28 14:24:56 +0900

    [#98] feature: generation output상태관리 추가
    Former-commit-id: c87513a38d2103f9d94c13df0bef1f7802364e75

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -6,6 +6,11 @@ import img2ImgSlice from './slices/generation/img2ImgSlice';
 import inpaintingSlice from './slices/generation/inpaintingSlice';
 import removeBgSlice from './slices/generation/removeBgSlice';
 import cleanupSlice from './slices/generation/cleanupSlice';
+import txt2ImgOutputSlice from './slices/generatedOutput/txt2ImgOutputSlice';
+import img2ImgOutputSlice from './slices/generatedOutput/img2ImgOutputSlice';
+import inpaintingOutputSlice from './slices/generatedOutput/inpaintingOutputSlice';
+import removeBgOutputSlice from './slices/generatedOutput/removeBgOutputSlice';
+import cleanupOutputSlice from './slices/generatedOutput/cleanupOutputSlice';
 import userInfoSlice from './slices/userInfoSlice';
 import trainingSlice from './slices/training/trainingSlice';
 
@@ -18,6 +23,11 @@ const store = configureStore({
     inpainting: inpaintingSlice,
     removeBg: removeBgSlice,
     cleanup: cleanupSlice,
+    txt2ImgOutput: txt2ImgOutputSlice,
+    img2ImgOutput: img2ImgOutputSlice,
+    inpaintingOutput: inpaintingOutputSlice,
+    removeBgOutput: removeBgOutputSlice,
+    cleanupOutput: cleanupOutputSlice,
     userInfo: userInfoSlice,
     training: trainingSlice
   }

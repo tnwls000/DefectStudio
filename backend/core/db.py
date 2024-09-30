0956901fb7a9cf3239ca0c3ea0bb260bcb2df838
SnowyPacks <originpc@naver.com>
2024-09-30 23:38:46 +0900

    HOTFIX : FE 브랜치에 필요하지 않는 파일 전부 제거
    Former-commit-id: 8c9b23306209d5b51ec7816a5cbb31b1fa14c88c

--- backend/core/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from core.config import settings
-
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=True) # echo -> prints SQL
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
-

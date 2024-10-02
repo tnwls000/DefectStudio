8b176a8bd20bdb035806d3601a15e6660b1bbfee
SnowySparks <originpc@naver.com>
2024-10-02 12:44:21 +0900

    [#125] feature: 배경화면 추가 및 다크모드에 대한 css설정
    Former-commit-id: be71c480545b20c2ca9b65c3fa706e931b32d454

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,9 +3,10 @@ import LoginForm from '../components/login/LoginForm';
 
 const Login = () => {
   return (
-    <div className="relative overflow-hidden w-full min-h-screen flex flex-row">
+    <div className="relative overflow-hidden w-full min-h-screen flex flex-row bg-white dark:bg-gray-700">
       {/* 움직이는 화면 넣는 곳 */}
       <section className="w-1/2 h-screen hidden lg:block bg-slate-400 relative">
+        <img src="https://picsum.photos/600/800" alt="" className="w-full h-full" />
         <p className="font-black text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[50px] ">
           Welcome
           <br />
@@ -18,13 +19,13 @@ const Login = () => {
       </section>
 
       {/* 로그인 영역 */}
-      <section className="w-full lg:w-1/2 flex flex-col justify-center items-center">
-        <p className="text-3xl font-black text-center text-black mb-6">Welcome to Defect Studio</p>
+      <section className="w-full lg:w-1/2 flex flex-col justify-center items-center text-black dark:text-white mb-6">
+        <p className="text-3xl font-black text-center ">Welcome to Defect Studio</p>
         <p className="text-base text-center mb-6">
-          <span className="text-base text-black">Don’t have an account? </span>
+          <span className="text-basefont-bold">Don’t have an account? </span>
 
           <Link to="/signup">
-            <span className="text-base font-black text-[#6200ea]">Sign up for free</span>
+            <span className="text-base font-black text-[#a490c0] hover:underline">Sign up for free</span>
           </Link>
         </p>
 

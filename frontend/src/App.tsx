3d436164dc9467596c8ce602b4dc04c20942095c
SnowySparks <originpc@naver.com>
2024-10-04 13:54:38 +0900

    [#130] refactor : createHashRouter 적용 및 Lazy Loading 적용. Loading Indicator 추가
    Former-commit-id: 20fcfd9ccf918686aed7752b08ae75097e3cdda1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,121 +1,257 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import Navbar from './components/common/Navbar';
-import Home from './pages/Home';
-import Generation from './pages/Generation';
-import Training from './pages/Training';
-import Model from './pages/Model';
-import Tokens from './pages/Tokens';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import PrivateRoute from './components/routing/PrivateRoute';
-import Settings from './pages/Settings';
-import Profile from './pages/Profile';
-import UserUpdate from './pages/UserUpdate';
-import EditProfile from './pages/EditProfile';
-import History from './pages/History';
+import { createHashRouter, Outlet, RouterProvider } from 'react-router-dom';
 
-function AppContent() {
-  return (
-    <>
-      <Navbar />
-      <div className="pt-[60px]">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+const Navbar = lazy(() => import('./components/common/Navbar'));
+const Home = lazy(() => import('./pages/Home'));
+const Generation = lazy(() => import('./pages/Generation'));
+const Training = lazy(() => import('./pages/Training'));
+const Model = lazy(() => import('./pages/Model'));
+const Tokens = lazy(() => import('./pages/Tokens'));
+const Login = lazy(() => import('./pages/Login'));
+const Signup = lazy(() => import('./pages/Signup'));
+const PrivateRoute = lazy(() => import('./components/routing/PrivateRoute'));
+const Settings = lazy(() => import('./pages/Settings'));
+const Profile = lazy(() => import('./pages/Profile'));
+const UserUpdate = lazy(() => import('./pages/UserUpdate'));
+const EditProfile = lazy(() => import('./pages/EditProfile'));
+const History = lazy(() => import('./pages/History'));
+import LoadingIndicator from './pages/LoadingIndicator';
 
-          {/* 로그인 안한경우 접근 불가 */}
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/generation/*"
-            element={
-              <PrivateRoute>
-                <Generation />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/history"
-            element={
-              <PrivateRoute>
-                <History />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/training"
-            element={
-              <PrivateRoute>
-                <Training />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/model"
-            element={
-              <PrivateRoute>
-                <Model />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/settings"
-            element={
-              <PrivateRoute>
-                <Settings />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tokens"
-            element={
-              <PrivateRoute>
-                <Tokens />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/profile/edit"
-            element={
-              <PrivateRoute>
-                <EditProfile />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/guestUserManage"
-            element={
-              <PrivateRoute>
-                <UserUpdate />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </div>
-    </>
-  );
-}
+const router = createHashRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Navbar />
+        <div className="pt-[60px]">
+          <Outlet />
+        </div>
+      </>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: 'login',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <Login />
+          </Suspense>
+        )
+      },
+      {
+        path: 'signup',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <Signup />
+          </Suspense>
+        )
+      },
+      {
+        path: 'generation/*',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <Generation />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: 'history',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <History />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: 'training',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <Training />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: 'model',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <Model />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: 'settings',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <Settings />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: '/tokens',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <Tokens />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: '/profile',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: '/profile/edit',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <EditProfile />
+            </PrivateRoute>
+          </Suspense>
+        )
+      },
+      {
+        path: '/guestUserManage',
+        element: (
+          <Suspense fallback={<LoadingIndicator />}>
+            <PrivateRoute>
+              <UserUpdate />
+            </PrivateRoute>
+          </Suspense>
+        )
+      }
+    ]
+  }
+]);
+
+// function AppContent() {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="pt-[60px]">
+//         <Routes>
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signup" element={<Signup />} />
+
+//           {/* 로그인 안한경우 접근 불가 */}
+//           <Route
+//             path="/"
+//             element={
+//               <PrivateRoute>
+//                 <Home />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/generation/*"
+//             element={
+//               <PrivateRoute>
+//                 <Generation />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/history"
+//             element={
+//               <PrivateRoute>
+//                 <History />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/training"
+//             element={
+//               <PrivateRoute>
+//                 <Training />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/model"
+//             element={
+//               <PrivateRoute>
+//                 <Model />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/settings"
+//             element={
+//               <PrivateRoute>
+//                 <Settings />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/tokens"
+//             element={
+//               <PrivateRoute>
+//                 <Tokens />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/profile"
+//             element={
+//               <PrivateRoute>
+//                 <Profile />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/profile/edit"
+//             element={
+//               <PrivateRoute>
+//                 <EditProfile />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/guestUserManage"
+//             element={
+//               <PrivateRoute>
+//                 <UserUpdate />
+//               </PrivateRoute>
+//             }
+//           />
+//         </Routes>
+//       </div>
+//     </>
+//   );
+// }
 
 function App() {
   return (
     <Provider store={store}>
-      <AppContent />
+      <RouterProvider router={router} />
     </Provider>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { HashRouter } from 'react-router-dom'; // BrowserRouter 대신 HashRouter 사용
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -11,9 +10,7 @@ export const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
